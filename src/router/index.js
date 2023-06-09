7a0b93f31a9b0621b21d2d86f6a064e8d0fc6f02
tkhrhys24 <tkhr.hys.24@gmail.com>
2023-06-09 14:32:33 +0900

    .

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: "/home",
     name: "home",
     component: HomeView,
   },
@@ -16,8 +16,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: HomeView,
   },
 ];
 
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : './',
+  //publicPath: process.env.NODE_ENV === 'production' ? '/' : './',
   outputDir: 'docs',
   transpileDependencies: ["vuetify"],
   filenameHashing: false, //ビルドのたびにファイルリネーム扱いになるのが気持ち悪いので false に設定する。
