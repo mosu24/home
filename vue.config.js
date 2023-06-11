eec4b0e268204804931e73693211522d6d0b5553
tkhrhys24 <tkhr.hys.24@gmail.com>
2023-06-11 16:03:29 +0900

    .

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-  outputDir: 'docs',
+  outputDir: 'dist',
   transpileDependencies: ["vuetify"],
   filenameHashing: false, //ビルドのたびにファイルリネーム扱いになるのが気持ち悪いので false に設定する。
 });
